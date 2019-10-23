b41d7e05c47ecd9506eaa3d8284762cbbd5d4923
Shubham Arora <shubhama1903@gmail.com>
2019-10-22 23:53:51 -0400

    added basic page style, filter panel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import FilterPanel from "./components/FilterPanel/FilterPanel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Digital Equity
-          <p>Brewing...</p>
-      </header>
+    <div className="App" style={{display: 'flex'}}>
+      <div style = {{'flexGrow': 1}}>
+        <FilterPanel></FilterPanel>
+      </div>
+      <div style = {{'flexGrow': 4}}>
+        <header className="App-header">
+            Digital Equity
+            <p>Brewing...</p>
+        </header>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/FilterPanel/FilterPanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './App.css';
+
+function FilterPanel() {
+  return (
+    <div className="filter-panel">
+        <ul>
+            <li>Filter 1</li>
+            <li>Filter 2</li>
+            <li>Filter 3</li>
+            <li>Filter 4</li>
+        </ul>
+    </div>
+  );
+}
+
+export default FilterPanel;
