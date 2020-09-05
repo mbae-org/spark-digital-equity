bec646d289cf6a3c4aab78b483e0214f09d591f3
Rishab Nayak <rishab@bu.edu>
2020-09-05 09:58:32 -0400

    added config Co-authored-by: Jane Feng <jfeng5529@bths.edu>

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from "@aws-amplify/ui-react"
 
+
 import {
     EntityType,
     EthnicityAcronymList,
